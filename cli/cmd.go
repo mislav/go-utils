34bcb2067daa962e270c7943a0e19777d6cb1193
Steffen Kötte <steffen.koette@gmail.com>
2016-04-19 17:28:58 +0200

    change Env to empty interface

--- cli/cmd.go
+++ cli/cmd.go
@@ -5,13 +5,17 @@ import (
 )
 
 // Cmd is the environment for each command of the CLI. It provides Stdout, Stderr
-// as well as the given flags
+// as well as the given flags and arguments
 type Cmd struct {
 	Args   *Args
 	Flags  *Flags
 	Stdout *ColoredWriter
 	Stderr *ColoredWriter
-	Env    map[string]string
+	Env    CommandConfig
+}
+
+// CommandConfig can store additional fields you want to pass to each command
+type CommandConfig interface {
 }
 
 // Exit should be called at the end of each command of the CLI to exit with the
@@ -29,7 +33,6 @@ func NewCmd(args *Args, flags *Flags) *Cmd {
 		Flags:  flags,
 		Stdout: NewColoredWriter(os.Stdout),
 		Stderr: stderr,
-		Env:    make(map[string]string),
 		Args:   args,
 	}
 }
