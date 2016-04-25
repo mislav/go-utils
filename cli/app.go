72f94db6bf93afad7e7f0d90427ade85f4adbab0
Steffen Kötte <steffen.koette@gmail.com>
2016-04-25 16:10:16 +0200

    add support for more help text for the commands register functions now can register more commands / flags

--- cli/app.go
+++ cli/app.go
@@ -7,20 +7,27 @@ var once sync.Once
 
 // App saves registered subcommands, flags and a bit more
 type App struct {
+	// DefaultCommandName: a command with this name will be called if no other name was provided
 	DefaultCommandName string
 	Version            string
 	commands           map[string]Command
-	Fallback           func(c *Cmd, cmdName string) ExitValue
-	Before             func(c *Cmd, cmdName string) string
-	flags              map[string]*Flag
+	// Fallback function will be called if no command with the given name was found
+	Fallback func(c *Cmd, cmdName string) ExitValue
+	// Before function will be called after cmd is initialize and can adjust it
+	// before it will be passed to the correct command
+	Before func(c *Cmd, cmdName string) string
+	flags  map[string]Flag
 }
 
 // Command is the struct for one subcommand of the app including help text and flags
 type Command struct {
-	Name     string
+	Name string
+	// Info is a short string shown behind the command in the help
+	Info string
+	// Help text will be shown after the info text if the user requests help for this command
 	Help     string
 	Function func(*Cmd) ExitValue
-	flags    map[string]*Flag
+	flags    map[string]Flag
 }
 
 // AppInstance returns the singleton instance of App
@@ -37,36 +44,42 @@ func (a *App) Commands() map[string]Command {
 }
 
 // Flags returns all registered application wide Flags
-func (a *App) Flags() map[string]*Flag {
+func (a *App) Flags() map[string]Flag {
 	return a.flags
 }
 
-// RegisterCommand registers a Command
-func (a *App) RegisterCommand(c Command) {
+// RegisterCommand registers Commands
+func (a *App) RegisterCommand(c ...Command) {
 	if a.commands == nil {
 		a.commands = make(map[string]Command)
 	}
-	a.commands[c.Name] = c
+	for _, command := range c {
+		a.commands[command.Name] = command
+	}
 }
 
-// RegisterFlag registers a Flag
-func (a *App) RegisterFlag(f Flag) {
+// RegisterFlag registers Flags
+func (a *App) RegisterFlag(f ...Flag) {
 	if a.flags == nil {
-		a.flags = make(map[string]*Flag)
+		a.flags = make(map[string]Flag)
 	}
-	a.flags[f.Long] = &f
+	for _, flag := range f {
+		a.flags[flag.Long] = flag
+	}
 }
 
-// RegisterFlag registers a Flag
-func (c *Command) RegisterFlag(f Flag) {
+// RegisterFlag registers Flags
+func (c *Command) RegisterFlag(f ...Flag) {
 	if c.flags == nil {
-		c.flags = make(map[string]*Flag)
+		c.flags = make(map[string]Flag)
 	}
-	c.flags[f.Long] = &f
+	for _, flag := range f {
+		c.flags[flag.Long] = flag
+	}
 }
 
 // Flags returns all registered Flags for the Command
-func (c *Command) Flags() map[string]*Flag {
+func (c *Command) Flags() map[string]Flag {
 	return c.flags
 }
 
@@ -82,11 +95,11 @@ func (a *App) Run(arguments []string) ExitValue {
 	parameters := &Parameters{}
 	var parameter *Parameter
 	for _, flag := range a.flags {
-		parameter, args = args.Extract(*flag)
+		parameter, args = args.Extract(flag)
 		parameters.AddParameter(parameter)
 	}
 	for _, flag := range command.flags {
-		parameter, args = args.Extract(*flag)
+		parameter, args = args.Extract(flag)
 		parameters.AddParameter(parameter)
 	}
 	cmd := NewCmd(args, parameters)
